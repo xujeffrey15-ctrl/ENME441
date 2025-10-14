e9f87176a085aaa3826b35b98d98958685f9beeb
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-14 13:54:26 -0400

    Update Bug.py

--- Bug.py
+++ Bug.py
@@ -8,25 +8,22 @@ Testing = Shifter.shifter(23,24,25)
 while True:
 	jumper = random.randint(0,1)
 	if jumper == 0:
-		if b <=128:
+		if b <= 128:
+			Testing.shiftByte(b)
 			b = b << 1
-			print(b)
-			print('gap')
-			print(jumper)
 			time.sleep(1)
 		else:
 			pass
 	elif jumper == 1:
-		if b >= 2:
+		if b >= 4:
+			Testing.shiftByte(b)
 			b = b >> 1
-			print(b)
-			print('gap')
-			print(jumper)
 			time.sleep(1)
 		else:
 			pass
 		
 	
+
 
 
 
