d431938af19605ab94e6df2db8717916172fe5e7
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-14 14:19:36 -0400

    Update Bug.py

--- Bug.py
+++ Bug.py
@@ -2,14 +2,10 @@ import random
 import Shifter
 import time
 
-b=8
-while True:
-	Testing = Shifter.shifter(23,24,25,b)
-	b += 1
-	Testing.shiftByte()
-	time.sleep(1)
-		
+Testing = Shifter.shifter(23,24,25)
+Testing.shiftByte(8)
 	
+
 
 
 
