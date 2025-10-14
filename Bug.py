6987f076f9037963d75cf8c3a2f4c8b46b8eba4f
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-14 13:41:26 -0400

    Update Bug.py

--- Bug.py
+++ Bug.py
@@ -9,12 +9,13 @@ while True:
 	jumper = random.randint(1,2)
 	if jumper == 1:
 		Testing.shiftByte(b)
-		b = b<<1
+		b<<1
 		time.sleep(0.5)
 	if jumper == 2:
 		Testing.shiftByte(b)
-		b = b>>1
+		b>>1
 		time.sleep(0.5)
+
 
 
 
