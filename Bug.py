f6ebc9e2f772a9d6627de3a24e964c50eb19b274
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-14 13:18:02 -0400

    Update Bug.py

--- Bug.py
+++ Bug.py
@@ -2,11 +2,13 @@ import random
 import time
 import Shifter
 
-Testing = Shifter.shifter(23,24,25,b=8)
+b=8
+Testing = Shifter.shifter(23,24,25,b)
 
 while True:
 	Testing.shiftByte(b)
 	b = b<<1
+
 
 
 
