085faff1ddfec49614b6acb088dc4784f3b37d36
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-14 14:18:47 -0400

    Update Shifter.py

--- Shifter.py
+++ Shifter.py
@@ -1,27 +1,29 @@
 import RPi.GPIO as GPIO
 import time
+GPIO.setmode(GPIO.BCM)
 
 class shifter:
-  def __init__ (self,serialPin,clockPin,latchPin,b):
+  def __init__ (self,serialPin,clockPin,latchPin):
     self.serialPin = serialPin
     self.clockPin = clockPin
     self.latchPin = latchPin
-    self.b = b
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(serialPin, GPIO.OUT)
-    GPIO.setup(latchPin, GPIO.OUT, initial=0)
-    GPIO.setup(clockPin, GPIO.OUT, initial=0)  
+    
+  def setup(self):
+    GPIO.setup(self.serialPin, GPIO.OUT)
+    GPIO.setup(self.latchPin, GPIO.OUT, initial=0)
+    GPIO.setup(self.clockPin, GPIO.OUT, initial=0)  
 
   def ping(self,p):
     GPIO.output(p,1)
     time.sleep(0)
     GPIO.output(p,0)
 
-  def shiftByte(self):
+  def shiftByte(self,b):
     for i in range(8):
-      GPIO.output(self.serialPin, self.b & (1<<i))
+      GPIO.output(self.serialPin, b & (1<<i))
       self.ping(self.clockPin)
     self.ping(self.latchPin)
+
 
 
 
