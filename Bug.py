a3e7781fefab04613aaadff262211e595fd643bd
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-14 12:01:15 -0400

    Add files via upload

---
+++ Bug.py
@@ -0,0 +1,15 @@
+import random
+import Shifter
+
+b = 8
+
+while True:
+	jumper = random.randint(1,2)
+	if jumper == 1:
+		Shifter(23,24,25,b)
+		b = b<<1
+		time.sleep(0.5)
+	elif jumper == 2:
+		Shifter(23,24,25,b)
+		b = b>>1
+		time.sleep(0.5)
