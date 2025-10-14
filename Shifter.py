997ebeef36a394cf3e23a3ca6f1791b4fd6f8bc3
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-14 12:47:32 -0400

    Update Shifter.py

--- Shifter.py
+++ Shifter.py
@@ -1,17 +1,18 @@
 class Shifter:
+  import RPi.GPIO as GPIO
+  import time
+  
   def __init__ (self,serialPin,clockPin,latchPin,b):
     self.serialPin = serialPin
     self.clockPin = clockPin
     self.latchPin = latchPin
     self.b = b
 
-  import RPi.GPIO as GPIO
-  import time
-  
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(self.serialPin, GPIO.OUT)
-  GPIO.setup(self.latchPin, GPIO.OUT, initial=0)
-  GPIO.setup(self.clockPin, GPIO.OUT, initial=0)  
+  def setup(self):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(self.serialPin, GPIO.OUT)
+    GPIO.setup(self.latchPin, GPIO.OUT, initial=0)
+    GPIO.setup(self.clockPin, GPIO.OUT, initial=0)  
 
   def __ping(self,p):
     GPIO.output(p,1)
@@ -23,6 +24,7 @@ class Shifter:
       GPIO.output(self.serialPin, self.b & (1<<i))
       __ping(self.clockPin)
     __ping(self.latchPin)
+
 
 
 
