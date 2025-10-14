1f3c26597d28e4a66bcea702d9c9d4211baf9210
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-14 13:11:08 -0400

    Update Bug.py

--- Bug.py
+++ Bug.py
@@ -8,13 +8,14 @@ Testing = Shifter.shifter(23,24,25,b)
 while True:
 	jumper = random.randint(1,2)
 	if jumper == 1:
-		Testing.shiftByte(b)
+		Testing.shiftByte()
 		b = b<<1
 		time.sleep(0.5)
 	elif jumper == 2:
-		Testing.shiftByte(b)
+		Testing.shiftByte()
 		b = b>>1
 		time.sleep(0.5)
+
 
 
 
