e1bc755321090226a11f7e5685271f3ea80e63f0
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-14 13:35:10 -0400

    Update Bug.py

--- Bug.py
+++ Bug.py
@@ -6,14 +6,15 @@ b=8
 Testing = Shifter.shifter(23,24,25)
 
 while True:
-	if b < 256:
+	if b != 256:
 		Testing.shiftByte(b)
 		b = b<<1
 		time.sleep(0.5)
-	elif b >= 2:
+	elif b != 1:
 		Testing.shiftByte(b)
 		b = b>>1
 		time.sleep(0.5)
+
 
 
 
