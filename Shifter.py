5c13cfa5a6684d9f1aaceca3ec347c5dfe55613e
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-14 11:15:12 -0400

    Add files via upload

---
+++ Shifter.py
@@ -0,0 +1,29 @@
+class Shifter(serialPin,clockPin,latchPin,b):
+  import RPi.GPIO as GPIO
+  import time
+  GPIO.setmode(GPIO.BCM)
+
+  GPIO.setup(serialPin, GPIO.OUT)
+  GPIO.setup(latchPin, GPIO.OUT, initial=0)  # start latch & clock low
+  GPIO.setup(clockPin, GPIO.OUT, initial=0)  
+
+  def __ping(p):
+    GPIO.output(p,1)
+    time.sleep(0)
+    GPIO.output(p,0)
+
+  def shiftByte(b):
+    for i in range(8):
+      GPIO.output(serialPin, b & (1<<i))
+      __ping(clockPin)
+    __ping(latchPin)
+
+import random
+b = random.randint(0,256)
+
+while True:
+  jump = random.randint(0,1)
+  if jump == 1:
+    Shifter(23,24,25,b<<1)
+  elif jump == 0:
+    Shifter(23,24,25,b>>1)
