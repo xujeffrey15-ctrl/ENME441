d26d5cca15887fbc6db7157f754d8bd5c9882c3f
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-14 11:37:29 -0400

    Update Shifter.py

--- Shifter.py
+++ Shifter.py
@@ -4,35 +4,34 @@ latchPin = 24
 import random
 b = random.randint(0,256)
 
-class Shifter(b):
-  import RPi.GPIO as GPIO
-  import time
-  GPIO.setmode(GPIO.BCM)
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
 
-  GPIO.setup(serialPin, GPIO.OUT)
-  GPIO.setup(latchPin, GPIO.OUT, initial=0)  # start latch & clock low
-  GPIO.setup(clockPin, GPIO.OUT, initial=0)  
+GPIO.setup(serialPin, GPIO.OUT)
+GPIO.setup(latchPin, GPIO.OUT, initial=0)  # start latch & clock low
+GPIO.setup(clockPin, GPIO.OUT, initial=0)  
 
-  def __ping(p):
-    GPIO.output(p,1)
-    time.sleep(0)
-    GPIO.output(p,0)
+def ping(p):
+  GPIO.output(p,1)
+  time.sleep(0)
+  GPIO.output(p,0)
 
-  def shiftByte(b):
-    for i in range(8):
-      GPIO.output(serialPin, b & (1<<i))
-      __ping(clockPin)
-    __ping(latchPin)
+def shiftByte(b):
+  for i in range(8):
+    GPIO.output(serialPin, b & (1<<i))
+    ping(clockPin)
+  ping(latchPin)
 
-testing = Shifter(b)
 while True:
   jump = random.randint(0,1)
   if jump == 1:
-    testing.Shifter()
+    shiftByte(b)
     b = b<<1
   elif jump == 0:
-    testing.Shifter()
+    shiftByte(b)
     b = b>>1
+
 
 
 
