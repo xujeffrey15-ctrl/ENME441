d6d1fde82902607fac403f19c2af37a37cff31f5
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-14 13:17:18 -0400

    Update Bug.py

--- Bug.py
+++ Bug.py
@@ -2,19 +2,11 @@ import random
 import time
 import Shifter
 
-b = 8
+Testing = Shifter.shifter(23,24,25,b=8)
 
 while True:
-	Testing = Shifter.shifter(23,24,25,b)
-	jumper = random.randint(1,2)
-	if jumper == 1:
-		Testing.shiftByte()
-		b = b<<1
-		time.sleep(0.5)
-	elif jumper == 2:
-		Testing.shiftByte()
-		b = b>>1
-		time.sleep(0.5)
+	Testing.shiftByte(b)
+	b = b<<1
 
 
 
