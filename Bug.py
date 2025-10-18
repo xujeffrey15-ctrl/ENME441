cef5b23f306ae50a6665e8f04df51a5627303c7b
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-18 09:16:39 -0400

    Update Bug.py

--- Bug.py
+++ Bug.py
@@ -4,9 +4,12 @@ import time
 
 Testing = Shifter.shifter(23,24,25)
 
-Testing.shiftByte(0b01100110)
-print("1")
-	
+try:
+	Testing.shiftByte(0b01100110)   # test out the new function
+	while 1: pass
+except:
+ 	GPIO.cleanup()
+
 
 
 
