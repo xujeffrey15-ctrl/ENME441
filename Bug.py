2a05d359ea126c440ed99e7a547fb51ef5722183
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-18 11:07:00 -0400

    Update Bug.py

--- Bug.py
+++ Bug.py
@@ -6,13 +6,14 @@ b = 8
 while True:
 	jumper = random.randint(1,2)
 	if jumper == 1:
-		Shifter(23,24,25)
+		Shifter(23,25,24)
 		b = b<<1
 		time.sleep(0.5)
 	elif jumper == 2:
-		Shifter(23,24,25)
+		Shifter(23,25,24)
 		b = b>>1
 		time.sleep(0.5)
+
 
 
 
