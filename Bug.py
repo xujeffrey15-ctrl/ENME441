5b50277d993c65cba5c5f68c75763e1206a459fe
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-19 09:54:08 -0400

    Update Bug.py

--- Bug.py
+++ Bug.py
@@ -3,21 +3,22 @@ import Shifter
 import time
 
 LEDS = {"1":1,"2":2,"3":4,"4":8,"5":16,"6":32,"7":64,"8":128}
+LightningBug = Shifter.shifter(23,25,24)
 
-class bug():
-	def __init__(self,LightningBug,timestep=0.05,x=3,isWrapOn=True):
+class Bug():
+	def __init__(self,LightningBug, timestep = 0.05, x = 3, isWrapOn = False):
 		self.LightningBug = LightningBug
 		self.timestep = timestep
 		self.isWrapOn = isWrapOn
 		self.x = x
-
-	def ShiftCall(self,b):
-		self.LightningBug.shiftByte(b)
-		time.sleep(self.timestep)
-
-	def bugging(self):
+		global b
 		b = LEDS[str(self.x)]
 
+	def ShiftCall(self,a):
+		self.LightningBug.shiftByte(a)
+		time.sleep(self.timestep)
+
+	def Bugging(self):
 		while True:
 			jumper = random.randint(0,1)
 			if self.isWrapOn == False:
@@ -38,10 +39,8 @@ class bug():
 			if self.isWrapOn == True:
 				if b < 1:
 					b = 128
-					self.ShiftCall(b)
 				if b > 128:
 					b = 1
-					self.ShiftCall(b)
 				else:
 					if jumper == 1:
 						b = b<<1
@@ -50,8 +49,40 @@ class bug():
 						b = b>>1
 						self.ShiftCall(b)
 
-Test = bug(Shifter.shifter(23,25,24))
-Test.bugging()
+	def stop(self):
+		self.Bugging(0)
+		print("Stopped")
+		break
+		
+	def start(self):
+		print("Started")
+		self.Bugging(b)
+
+	def ChangeSpeed(self,r):
+		timestep = timestep/r
+
+	def ChangeWrap(self,boo):
+		isWrapOn = boo
+s1 = 17
+s2 = 27
+s3 = 22
+GPIO.setup(s1, GPIO.IN)
+GPIO.setup(s2, GPIO.IN)
+GPIO.setup(s3, GPIO.IN)
+BugSet = Bug(Shifter.shifter(23,25,24))
+
+if GPIO.INPUT(s1,1):
+	BugSet.start()
+if GPIO.INPUT(s1,0):
+	BugSet.stop()
+if GPIO.INPUT(s2,1):
+	BugSet.ChangeSpeed(3)
+if GPIO.INPUT(s2,0):
+	BugSet.ChangeSpeed(1)
+if GPIO.INPUT(s3,1):
+	BugSet.ChangeWrap(True)
+if GPIO.INPUT(s3,0):
+	BugSet.ChangeWrap(False)
 
 
 
