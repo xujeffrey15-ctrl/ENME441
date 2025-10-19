86522b1b2fdc2a5ebe75eb84283b9dcaeede052d
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-19 17:37:19 -0400

    Update bug1.py

--- bug1.py
+++ bug1.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 import Bug
 
 (s1,s2,s3) = (17,27,22)
-Buggg = Bug.Bugg()
+Buggg = Bug.Bugg(Shifter.shifter(23,25,24))
 GPIO.setmode(GPIO.BCM) 
 GPIO.setup(s1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(s2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -14,3 +14,4 @@ while True:
 		Buggg.stop()
 	if GPIO.input(s1) == 1:
 		Buggg.start()
+
