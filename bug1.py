d714e48ea7e3e90661ee4a617b00002a4813832c
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-19 17:32:53 -0400

    Add files via upload

---
+++ bug1.py
@@ -0,0 +1,16 @@
+import threading
+import RPi.GPIO as GPIO
+import Bug
+
+(s1,s2,s3) = (17,27,22)
+Buggg = Bug.Bugg()
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(s1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(s2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(s3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+while True:
+	if GPIO.input(s1) == 0:
+		Buggg.stop()
+	if GPIO.input(s1) == 1:
+		Buggg.start()
