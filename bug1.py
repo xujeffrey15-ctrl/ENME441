80f6a8b96a4bb3afe13c1039f759928469c3a7b2
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-19 18:18:53 -0400

    Update bug1.py

--- bug1.py
+++ bug1.py
@@ -10,6 +10,13 @@ GPIO.setup(s1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(s2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(s3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+LTB = Bug()
+
+while True:
+  if GPIO.input(s1) == 0:
+    LTB.stop()
+
+
 
 
 
