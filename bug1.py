3b3c07264f90b823971c90fdb056f27fe3e5e15c
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-19 17:55:55 -0400

    Update bug1.py

--- bug1.py
+++ bug1.py
@@ -1,19 +1,15 @@
 import threading
 import RPi.GPIO as GPIO
 import Bug
-import Shifter
 
 (s1,s2,s3) = (17,27,22)
-Buggg = Bug.Bugg((23,25,24))
 GPIO.setmode(GPIO.BCM) 
 GPIO.setup(s1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(s2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(s3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-while True:
-	print(GPIO.input(s1))
-	if GPIO.input(s1) == 0:
-		Buggg.stop()
+LTB = Bug.Bugg()
+
 
 
 
