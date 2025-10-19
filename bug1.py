6e52dd612b0f223822ae90d14854305e3191c829
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-19 18:03:35 -0400

    Update bug1.py

--- bug1.py
+++ bug1.py
@@ -2,13 +2,14 @@ import threading
 import RPi.GPIO as GPIO
 from Bug import Bugg
 
-M = Bugg()
+GPIO.cleanup()
 
 (s1,s2,s3) = (17,27,22)
 GPIO.setmode(GPIO.BCM) 
 GPIO.setup(s1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(s2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(s3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 
 
 
