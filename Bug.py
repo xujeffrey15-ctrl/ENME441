ed28fb5a5b6d7fea5b27eafe634d58e97b9cf4cd
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-19 17:36:16 -0400

    Update Bug.py

--- Bug.py
+++ Bug.py
@@ -1,24 +1,15 @@
 import random
 import Shifter
 import time
-import threading
-import RPi.GPIO as GPIO
-
-(s1,s2,s3) = (17,27,22)
-GPIO.setmode(GPIO.BCM) 
-GPIO.setup(s1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(s2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(s3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 LEDS = {"1":1,"2":2,"3":4,"4":8,"5":16,"6":32,"7":64,"8":128}
 
-class Bug():
+class Bugg():
 	def __init__(self,LightningBug, timestep = 0.05, x = 3, isWrapOn = False):
 		self.LightningBug = LightningBug
 		self.timestep = timestep
 		self.isWrapOn = isWrapOn
 		self.x = x
-
 
 	def ShiftCall(self,bytedata):
 		self.LightningBug.shiftByte(bytedata)
@@ -58,27 +49,22 @@ class Bug():
 			if self.isWrapOn == True:
 				self.UnboundedJump(jumper)
 
-	def stop(self):
-		BugThread.join()
-		self.ShiftCall(0)
-		print("Stopped")
-		
-	def start(self):
-		print("Started")
-		BugThread.start()
+	def BugThread(self):
+		ThreadBug = threading.Thread(target = self.Bugging(), daemon = True)
 
 	def ChangeSpeed(self,r):
-		Bug.timestep = self.timestep/r
+		self.timestep = 0.05/r
 
-	def ChangeWrap(self,boo):
-		Bug.isWrapOn = boo
+	def ChangeWrap(self,b):
+		self.isWrapOn = b
 
-BugSet = Bug(Shifter.shifter(23,25,24))
-BugThread = threading.Thread(target=(BugSet.Bugging))
-BugThread.daemon = True
+	def start(self):
+		self.ThreadBug.start()
 
-GPIO.add_event_detect(s1, GPIO.RISING, callback=BugSet.start(), bouncetime = 500)
-GPIO.add_event_detect(s1, GPIO.FALLING, callback=BugSet.stop(), bouncetime = 500)
+	def stop(self):
+		self.ThreadBug.join()
+		self.ShiftCall(0)
+
 
 
 
