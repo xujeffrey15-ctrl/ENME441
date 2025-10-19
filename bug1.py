2fc9b28ee55c0310f82ca8a26c41466821ab3069
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-19 17:54:02 -0400

    Update bug1.py

--- bug1.py
+++ bug1.py
@@ -4,7 +4,7 @@ import Bug
 import Shifter
 
 (s1,s2,s3) = (17,27,22)
-Buggg = Bug.Bugg()
+Buggg = Bug.Bugg((23,25,24))
 GPIO.setmode(GPIO.BCM) 
 GPIO.setup(s1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(s2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -14,6 +14,7 @@ while True:
 	print(GPIO.input(s1))
 	if GPIO.input(s1) == 0:
 		Buggg.stop()
+
 
 
 
