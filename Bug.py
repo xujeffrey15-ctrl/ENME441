513db64c0c754f018ae216fad68e970cb442dd57
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-20 07:48:09 -0400

    Update Bug.py

--- Bug.py
+++ Bug.py
@@ -5,9 +5,7 @@ import threading
 import RPi.GPIO as GPIO
 
 LEDS = {"1":1,"2":2,"3":4,"4":8,"5":16,"6":32,"7":64,"8":128}
-s1 = 17
 GPIO.setmode(GPIO.BCM) 
-GPIO.setup(s1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 class Bugg():
 	@classmethod
@@ -67,6 +65,38 @@ class Bugg():
 		while GPIO.input(s1) == False:
 			pass
 
+s1 = 17
+s2 = 19
+s3 = 26
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(s1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(s2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(s3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+LTB = Bugg()
+      
+def sensor2():
+   if GPIO.input(s2) == True:
+      LTB.ChangeSpeed(1)
+   if GPIO.input(s2) == False:
+      LTB.ChangeSpeed(10)
+
+def sensor3():
+   if GPIO.input(s3) == True:
+      LTB.ChangeWrap(True)
+   if GPIO.input(s3) == False:
+      LTB.ChangeWrap(False)
+      
+try:
+   while True:
+      if GPIO.input(s1) == True:
+         LTB.Start()
+         sensor2()
+         sensor3()
+      else:
+         LTB.stop()
+except KeyboardInterrupt:
+   GPIO.cleanup()
 
 
 
