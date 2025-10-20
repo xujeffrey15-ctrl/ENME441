2ad3d2fd39d20c190acd8226aae9ca8a4f0599e5
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-10-19 20:31:26 -0400

    Update bug1.py

--- bug1.py
+++ bug1.py
@@ -9,11 +9,12 @@ GPIO.setup(s2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(s3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 LTB = Bugg()
-GPIO.add_event_detect(s1, GPIO.FALLING, callback=Bugg.stop, bouncetime=1000)
+GPIO.add_event_detect(s1, GPIO.FALLING, callback=LTB.stop, bouncetime=1000)
 
 while True:
-   Bugg.Bugging()
+   LTB.Bugging()
   
+
 
 
 
