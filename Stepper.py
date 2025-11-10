02d90e55e6b296ac1bb8df498dbd6395d19d25d1
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-11-10 16:35:11 -0500

    Update Stepper.py

--- Stepper.py
+++ Stepper.py
@@ -1,126 +1,51 @@
+# stepper_with_shifter.py
+#
+# Drive a stepper motor through a shift register
+#
+# Note that the 5V 28BYJ-48 stepper motors can be driven directly
+# from the shift register outputs (35mA @ 5V for SN74HC595)
+#
+# Use Vcc = 5V for both the shift register & motor.
+#
+# While this code is designed for driving a single motor,
+# it can be extended to work with 2+ motors without
+# using any additional GPIO pins.
+
+from Shifter import shifter
 import time
-import multiprocessing
-from Shifter import shifter   # our custom Shifter class
 
-class Stepper:
-    """
-    Supports operation of an arbitrary number of stepper motors using
-    one or more shift registers.
-  
-    A class attribute (shifter_outputs) keeps track of all
-    shift register output values for all motors.  In addition to
-    simplifying sequential control of multiple motors, this schema also
-    makes simultaneous operation of multiple motors possible.
-   
-    Motor instantiation sequence is inverted from the shift register outputs.
-    For example, in the case of 2 motors, the 2nd motor must be connected
-    with the first set of shift register outputs (Qa-Qd), and the 1st motor
-    with the second set of outputs (Qe-Qh). This is because the MSB of
-    the register is associated with Qa, and the LSB with Qh (look at the code
-    to see why this makes sense).
- 
-    An instance attribute (shifter_bit_start) tracks the bit position
-    in the shift register where the 4 control bits for each motor
-    begin.
-    """
+s = shifter(16,21,20)   # Set up shifter
 
-    # Class attributes:
-    num_steppers = 0      # track number of Steppers instantiated
-    shifter_outputs = 0   # track shift register outputs for all motors
-    seq = [0b0001,0b0011,0b0010,0b0110,0b0100,0b1100,0b1000,0b1001] # CCW sequence
-    delay = 1200          # delay between motor steps [us]
-    steps_per_degree = 4096/360    # 4096 steps/rev * 1/360 rev/deg
+cycle = [0b0001,
+         0b0011,
+         0b0010,
+         0b0110,
+         0b0100,
+         0b1100,
+         0b1000,
+         0b1001]
 
-    def __init__(self, shifter, lock):
-        self.s = shifter           # shift register
-        self.angle = 0             # current output shaft angle
-        self.step_state = 0        # track position in sequence
-        self.shifter_bit_start = 4*Stepper.num_steppers  # starting bit position
-        self.lock = lock           # multiprocessing lock
+# track position within m_seq:
+pos = 0
 
-        Stepper.num_steppers += 1   # increment the instance count
+delay = 1200/1e6  # delay between steps [us]
+# Make a full rotation of the output shaft:
+def loop(dir): # dir = rotation direction (1=cww, -1=cw)
+    global pos
+    for i in range(4096): # 4096 steps/rev
+        pos += dir
+        pos %= 8 
+        s.shiftByte(cycle[pos]<<4)
+        time.sleep(delay)
 
-    # Signum function:
-    def __sgn(self, x):
-        if x == 0: return(0)
-        else: return(int(abs(x)/x))
-
-    # Move a single +/-1 step in the motor sequence:
-    def __step(self, dir):
-        self.step_state += dir    # increment/decrement the step
-        self.step_state %= 8      # ensure result stays in [0,7]
-        Stepper.shifter_outputs |= 0b1111<<self.shifter_bit_start
-        Stepper.shifter_outputs &= Stepper.seq[self.step_state]<<self.shifter_bit_start
-        self.s.shiftByte(Stepper.shifter_outputs)
-        self.angle += dir/Stepper.steps_per_degree
-        self.angle %= 360         # limit to [0,359.9+] range
-
-    # Move relative angle from current position:
-    def __rotate(self, delta):
-        self.lock.acquire()                 # wait until the lock is available
-        numSteps = int(Stepper.steps_per_degree * abs(delta))    # find the right # of steps
-        dir = self.__sgn(delta)        # find the direction (+/-1)
-        for s in range(numSteps):      # take the steps
-            self.__step(dir)
-            time.sleep(Stepper.delay/1e6)
-        self.lock.release()
-
-    # Move relative angle from current position:
-    def rotate(self, delta):
-        time.sleep(0.1)
-        p = multiprocessing.Process(target=self.__rotate, args=(delta,))
-        p.start()
-
-    def goAngle(self, angle):
-        angle %= 360
-        delta = angle - self.angle
-        if delta > 180:
-            delta -= 360
-        elif delta <= -180:
-            delta += 360
-        self.rotate(delta)
-        time.sleep(0.5)
-
-    # Set the motor zero point
-    def zero(self):
-        self.angle = 0
-
-
-# Example use:
-
-if __name__ == '__main__':
-
-    s = shifter(16,21,20)   # set up Shifter
-
-    # Use multiprocessing.Lock() to prevent motors from trying to 
-    # execute multiple operations at the same time:
-    lock = multiprocessing.Lock()
-
-    # Instantiate 2 Steppers:
-    m1 = Stepper(s, lock)
-    m2 = Stepper(s, lock)
-
-    # Zero the motors:
-    m1.zero()
-    m2.zero()
-
-    # Move as desired, with eacg step occuring as soon as the previous 
-    # step ends:
-    m1.goAngle(-90)
-    m1.goAngle(45)
-
-    # If separate multiprocessing.lock objects are used, the second motor
-    # will run in parallel with the first motor:
- 
-    # While the motors are running in their separate processes, the main
-    # code can continue doing its thing: 
-    try:
-        while True:
-            pass
-    except:
-        print('\nend')
+try:
+    loop(1)
+    loop(-1)
+except Exception as e:
+    print(e)
 
    
+
 
 
 
