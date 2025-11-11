1963d7faf50b5778a0b07f3e642f609a4a247abe
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-11-11 14:23:42 -0500

    Update StepperMulti.py

--- StepperMulti.py
+++ StepperMulti.py
@@ -27,8 +27,8 @@ class Stepper:
     def __step(self, dir):
         self.step_state += dir    # increment/decrement the step
         self.step_state %= 8      # ensure result stays in [0,7]
-        Stepper.shifter_outputs |= 0b1111<<self.shifter_bit_start
         Stepper.shifter_outputs &= Stepper.seq[self.step_state]<<self.shifter_bit_start
+        Stepper.shifter_outputs |= 0b1111<<self.shifter_bit_start
         self.s.shiftByte(Stepper.shifter_outputs)
         self.angle += dir/Stepper.steps_per_degree
         self.angle %= 360         # limit to [0,359.9+] range
@@ -99,6 +99,7 @@ if __name__ == '__main__':
             pass
     except:
         print('\nend')
+
 
 
 
