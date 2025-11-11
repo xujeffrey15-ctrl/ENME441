82339d2e1c22aebc1aa3d3b1ff376a14e6214dcb
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-11-11 14:01:32 -0500

    Update StepperMulti.py

--- StepperMulti.py
+++ StepperMulti.py
@@ -36,7 +36,7 @@ class Stepper:
     num_steppers = 0      # track number of Steppers instantiated
     shifter_outputs = 0   # track shift register outputs for all motors
     seq = [0b0001,0b0011,0b0010,0b0110,0b0100,0b1100,0b1000,0b1001] # CCW sequence
-    delay = 1200          # delay between motor steps [us]
+    delay = 12000          # delay between motor steps [us]
     steps_per_degree = 4096/360    # 4096 steps/rev * 1/360 rev/deg
 
     def __init__(self, shifter, lock):
@@ -129,4 +129,5 @@ if __name__ == '__main__':
     except:
 
         print('\nend')
+
 
