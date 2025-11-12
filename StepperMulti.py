e460be2559e8823cf7cb20fb290bfa16fbca35e5
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-11-12 12:56:08 -0500

    Update StepperMulti.py

--- StepperMulti.py
+++ StepperMulti.py
@@ -5,6 +5,7 @@ from Shifter import shifter  # your custom module
 # Shared array for two steppers (integers)
 myArray = multiprocessing.Array('i', 2)
 
+
 class Stepper:
     seq = [0b0001, 0b0011, 0b0010, 0b0110,
            0b0100, 0b1100, 0b1000, 0b1001]
@@ -19,6 +20,11 @@ class Stepper:
         self.step_state = 0
         self.shifter_bit_start = 4 * index
         self.q = multiprocessing.Queue()
+
+        # Start a dedicated process to run commands from the queue
+        self.proc = multiprocessing.Process(target=self._run)
+        self.proc.daemon = True  # ends when main program ends
+        self.proc.start()
 
     def _sgn(self, x):
         return 0 if x == 0 else int(abs(x)/x)
@@ -50,21 +56,25 @@ class Stepper:
         for _ in range(steps):
             self._step(direction)
 
-    def rotate(self, delta):
-        delta = self.q.get()
-        p = multiprocessing.Process(target=self._rotate, args=(delta,))
-        p.start()
+    def _run(self):
+        """ Continuously run commands from the queue """
+        while True:
+            delta = self.q.get()  # blocks until a new command
+            self._rotate(delta)
 
     def zero(self):
         self.angle = 0
 
-    def goAngle(self, angle):
-        diff = angle - self.angle
-        if abs(diff) <= 180:
-            pass
-        elif abs(diff) > 180:
-            angle = -1*(360-(angle-self.angle))
-        self.q.put(angle)
+    def goAngle(self, target_angle):
+        """ Calculate shortest path and queue the movement """
+        diff = target_angle - self.angle
+        # Choose shortest rotation
+        if diff > 180:
+            diff -= 360
+        elif diff < -180:
+            diff += 360
+        self.q.put(diff)
+
 
 if __name__ == '__main__':
     s = shifter(16, 21, 20)
@@ -73,15 +83,30 @@ if __name__ == '__main__':
     m1 = Stepper(s, lock, 0)
     m2 = Stepper(s, lock, 1)
 
-    # Start both motors at once
-
+    # Initialize angles
     m1.zero()
     m2.zero()
+
+    # Queue multiple commands
     m1.goAngle(90)
     m1.goAngle(-45)
     m1.goAngle(-135)
     m1.goAngle(135)
     m1.goAngle(0)
+
+    m2.goAngle(-90)
+    m2.goAngle(45)
+    m2.goAngle(135)
+    m2.goAngle(-135)
+    m2.goAngle(0)
+
+    # Keep main program running to let motors finish
+    try:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print("\nExiting")
+
 
 
 
