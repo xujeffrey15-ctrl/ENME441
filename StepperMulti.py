547f407a27b1d21311cd6314b5e605ec941fa16f
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-11-12 06:40:22 -0500

    Update StepperMulti.py

--- StepperMulti.py
+++ StepperMulti.py
@@ -34,8 +34,6 @@ class Stepper:
             self.myValue.value |= Stepper.shifter_outputs
             self.angle += dir/Stepper.steps_per_degree
             self.angle %= 360
-            print(bin(self.myValue.value))
-            time.sleep(0.5)
 
     # Move relative angle from current position:
     def __rotate(self, delta):
@@ -82,6 +80,7 @@ if __name__ == '__main__':
             pass
     except:
         print('\nend')
+
 
 
 
