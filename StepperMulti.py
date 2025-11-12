51cd61ff2d655981d4e2073c8113edd1815c23ee
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-11-12 08:12:36 -0500

    Update StepperMulti.py

--- StepperMulti.py
+++ StepperMulti.py
@@ -37,6 +37,8 @@ class Stepper:
             self.s.shiftByte(myArray[Stepper.num_steppers-1])
             self.angle += dir/Stepper.steps_per_degree
             self.angle %= 360
+            print(bin(myArrayStepper.num_steppers-1));
+            time.sleep(1)
             myArray[Stepper.num_steppers-1] = 0b0000<<self.shifter_bit_start
 
     # Move relative angle from current position:
@@ -80,6 +82,7 @@ if __name__ == '__main__':
             pass
     except:
         print('\nend')
+
 
 
 
