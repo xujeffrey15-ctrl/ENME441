8917e05ea71e28b7784352a3518ece1aaa7ea5b6
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-11-29 09:48:30 -0500

    Update ProjectCode_withSaved.JsonFile.py

--- ProjectCode_withSaved.JsonFile.py
+++ ProjectCode_withSaved.JsonFile.py
@@ -26,7 +26,8 @@ def compute_angles(target_x, target_y, target_z=0):
     xy_angle = math.degrees(math.atan2(dy, dx))
 
     # Z angle (0 if target_z=0)
-    z_angle = math.degrees(math.atan2(target_z, horiz))
+    angle_z = math.degrees(math.atan2(dz, horiz))
+    angle_z = (180 - angle_z) % 360
 
     return xy_angle, z_angle
 
@@ -64,6 +65,7 @@ AngleConversion()
 
 print("\nXY Angles:", goanglexy)
 print("\nZ Angles:", goanglez)
+
 
 
 
