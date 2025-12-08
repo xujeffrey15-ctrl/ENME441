83534126ed489bf3dbf19281150bf9db2031398e
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-12-08 17:51:45 -0500

    Update Stich_Code.py

--- Stich_Code.py
+++ Stich_Code.py
@@ -1,6 +1,21 @@
 #Code to stitch together JSON Reader and Motor Code while recording crucial data for calibration
+
+#Imports
 import Json_Reader
 import multiprocessing 
+
+#Global variables
+XY = Json_Reader.goanglexy                        #Calculated in-plane rotations
+Z = Json_Reader.goanglez                          #Calculated out-of-plane rotations
+numturrets = len(Json_Reader.TurretData)          #Number of turrets for automation
+numball = len(Json_Reader.BallData)               #Number of stationary ball targets for automation
+
+class Stepper_Motors():
+    def Motor_To_Shift_Register(self, desired_angle):            #Converts both desired angles to a single string of 8 bit binary code for shift register
+        #Has to be able to take in angle requests and combine the results from both process into a single 8 bit string
+        Motor_wiring = [
+        
+        
 
 class calibration():
     def self_calibration(): #Can be used if you know the magnitude and direction of the previous rotation
@@ -12,6 +27,5 @@ class calibration():
             #motor continously rotate between 1.5 times the other direction, eventually it should rest close to 90 which is the desired starting line
         #Preferably the code will interupt the automation code
 
-    def engage_motors():    #Ideally this code takes on the multithreading instead of Motor_Code for better control. Ideally this code will be able to start two independent motor code and then join them when done
-        calibration_thread.start()
+
         
