0d7b72b5b8feca10867d8ad7df3bd46fe12ff38d
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-12-08 20:18:04 -0500

    Update Stich_Code.py

--- Stich_Code.py
+++ Stich_Code.py
@@ -2,6 +2,7 @@
 
 #Imports
 from Motor_Code_Project import Stepper
+import RPi.GPIO as GPIO
 import Json_Reader
 import multiprocessing
 
@@ -13,33 +14,48 @@ numball = len(Json_Reader.BallData)               #Number of stationary ball tar
 
 class Stepper_Motors():
    def __init__(self):
-        self.s = shifter(16, 21, 20)
-        self.lock = multiprocessing.Lock()
-        self.m1 = Stepper(self.s, self.lock, 0)
-        self.m2 = Stepper(self.s, self.lock, 1)
+      self.s = shifter(16, 21, 20)
+      self.lock = multiprocessing.Lock()
+      self.m1 = Stepper(self.s, self.lock, 0)
+      self.m2 = Stepper(self.s, self.lock, 1)
+      self.angle = 0
 
-   def Calibration():
+   def Calibration(self,toggle):
       calibration_magnitude = 120
-      calibration_direction = 1
+      if toggle == 1:
+         # Calibrate motors to 90 degrees
+         while abs((self.angle - 90)) >= 0.5:
+            self.m1.goAngle(calibration_magnitude)
+            self.m2.goAngle(calibration_magnitude)
+            self.m1.both.wait()
+            self.m2.both.wait()
+            self.angle += calibration_magnitude
+            calibration_magnitude = (3*(calibration_magnitude - self.angle))/2 
+            print(self.angle)
+         print("Motors Calibrated")
 
-      # Calibrate motors to 90 degrees
-      while calibration_magnitude >= 0.5:
-         self.m1.goAngle(calibration_magnitude)
-         self.m2.goAngle(calibration_magnitude)
-         calibration_magnitude = calibration_magnitude + calibration_magnitude/2
-         calibration_direction = -calibration_direction
-      print("Motors Calibrated")
+         # Test Laser
+         GPIO.output(11,1)
+         time.sleep(3)
+         GPIO.output(11,0)
+         print("Laser Primed")
+      else:
+         pass
 
-      # Test Laser
-      GPIO.output(11,1)
-      time.sleep(3)
-      GPIO.output(11,0)
-      print("Laser Primed")
+   def Manual_Motors(self,toggle,diff):
+      if toggle == 1:
+         self.m1.goAngle(diff)
+         self.m2.goAngle(diff)
+         self.m1.both.wait()
+         self.m2.both.wait()
+         print("Motors rotation completed")
+      else:
+         pass
    
-   def Automated_Motors():
+   def Automated_Motors(self):
       for turrets in range(1, numturrets):
-         self.m1.goAngle(XY[f"turret_{t}"])
-         self.m2.goAngle(Z[f"turret_{t}"])
+         self.m1.goAngle(XY[f"turret_{turrets}"])
+         self.m2.goAngle(Z[f"turret_{turrets}"])
          self.m1.both.wait()
          self.m2.both.wait()
 
@@ -49,8 +65,8 @@ class Stepper_Motors():
          GPIO.output(11,0)
     
       for balls in range(1, numball):
-         self.m1.goAngle(XY[f"ball_{b}"])
-         self.m2.goAngle(Z[f"ball_{b}"])
+         self.m1.goAngle(XY[f"ball_{balls}"])
+         self.m2.goAngle(Z[f"ball_{balls}"])
          self.m1.both.wait()
          self.m2.both.wait()
     
