68ffb0d4a2091d5fd443076f52fbe0cb9356b2d1
xujeffrey15-ctrl <xujeffrey15@gmail.com>
2025-12-13 01:17:27 -0500

    Update Json_Reader.py

--- Json_Reader.py
+++ Json_Reader.py
@@ -17,29 +17,53 @@ print(data)
 TurretData = data["turrets"]        # dict of turret objects
 BallData = data["globes"]           # list of ball objects
 
-# We are team 18
-ownxcoord = 300 * math.cos(1.117010721276371)
-ownycoord = 300 * math.sin(1.117010721276371)
-
 goanglexy = {}
 goanglez = {}
 
+#Team 18
+Own_R_Value = 300
+Own_Theta_Value = TurretData["18"]["theta"]
+Own_X_Coord = 300*math.cos(theta)
+Own_Y_Coord = 300*math.sin(theta)
+Own_Z_Coord = 0
+
 def compute_angles(target_x, target_y, target_z=0):
 
-    dx = target_x - ownxcoord
-    dy = target_y - ownycoord
-    horiz = math.sqrt(dx*dx + dy*dy)
+    #For In Plane Rotations
+    Side_a = (Previous_Y - Own_Y_Coord)/(Previous_X - Own_X_Coord)
+    Side_b = (Target_Y - Previous_Y)/(Target_X - Previous_Y)
+    side_c = (Own_Y_Coord - Target_Y)/(Own_X_Coord - Target_X)
+    Angle_diff_x = arccos((((Side_b)**2) - ((Side_a)**2) - ((Side_c)**2))/(-2*Side_C*Side_a))
 
-    # XY angle
-    xy_angle = math.degrees(math.atan2(dy, dx))
+    if Target_Y > Previous_Y:
+        Angle_diff_x = - Angle_diff_x
+    if Target Y <= Previous_Y:
+        pass
 
-    # Z angle (0 if target_z=0)
-    z_angle = math.degrees(math.atan2(target_z, horiz))
+    #For Out of Plane Rotations
+    Side_z = Target_z - Previous_z
+    Height_Hypotenuse = sqrt((Side_c**2) + (Side_z**2))
+    Angle_diff_z = arccos(((Side_z**2)-(Side_c**2) - (Height_Hypotenuse**2))/(-2*Side_c*Height_Hypotenuse))
 
-    return xy_angle, z_angle
+    if Target_z >= Previous_z:
+        pass
+    if Target_z < Previous_z:
+        Angle_diff_z = - Angle_diff_z
+
+    return Angle_diff_x, Angle_diff_z
 
 def AngleConversion():
 
+    #Have to be able to store previous data
+    #Have to be able to parse through target data
+    #Find the angle difference
+
+    #equation of the circle --> y^2 + x^2 = 300^2
+    #y = sqrt(300^2 - x^2)
+    #dy/dx for when x = own_x_coord --> gives m
+    #y = mx+b --> 
+    
+    
     # ---- TURRETS ----
     for tnum, tinfo in TurretData.items():
         r = tinfo["r"]
@@ -72,6 +96,7 @@ AngleConversion()
 
 print("\nXY Angles:", goanglexy)
 print("\nZ Angles:", goanglez)
+
 
 
 
